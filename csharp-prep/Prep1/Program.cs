87fcb31560f1e3d356c06d01f1f5efb3f27c2968
Preston Jobes <grouchypj6921@gmail.com>
2024-01-12 16:36:57 -0900

    changed Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name?" );
+        String first = Console.ReadLine();
+
+        Console.Write("what is your las name?" );
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"your name is {last},{first} {last}.");
     }
 }
